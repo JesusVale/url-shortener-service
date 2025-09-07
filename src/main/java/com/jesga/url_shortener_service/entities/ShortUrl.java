d18b8ab2a1a9ca0f9734a163079a76e55ecb0843
JesusVale <108861698+JesusVale@users.noreply.github.com>
2025-09-06 19:32:11 -0700

    project setup

---
+++ src/main/java/com/jesga/url_shortener_service/entities/ShortUrl.java
@@ -0,0 +1,35 @@
+package com.jesga.url_shortener_service.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ShortUrl {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String shortCode;
+
+    private String originalUrl;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime expiresAt;
+
+    private Integer clickCount;
+
+}
