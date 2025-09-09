fd65bb8f7800f3b9cc4ae59d1889c2ce9aa1c500
JesusVale <108861698+JesusVale@users.noreply.github.com>
2025-09-08 19:42:46 -0700

    fix: Redis configuration serialization error

--- src/main/java/com/jesga/url_shortener_service/config/RedisConfig.java
+++ src/main/java/com/jesga/url_shortener_service/config/RedisConfig.java
@@ -1,12 +1,16 @@
 package com.jesga.url_shortener_service.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.jesga.url_shortener_service.entities.ShortUrl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
@@ -17,11 +21,20 @@ public class RedisConfig {
 
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+
+        Jackson2JsonRedisSerializer<ShortUrl> serializer = new Jackson2JsonRedisSerializer<>(objectMapper, ShortUrl.class);
+
         RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(60)) // ttl = time to leave. After 60 minutes of an entry being cached it will expire
                 .disableCachingNullValues()
                 .serializeValuesWith(RedisSerializationContext.SerializationPair
-                        .fromSerializer(new Jackson2JsonRedisSerializer<>(ShortUrl.class)));
+                        .fromSerializer(serializer));
 
         return RedisCacheManager
                 .builder(connectionFactory).cacheDefaults(redisCacheConfiguration).build();
