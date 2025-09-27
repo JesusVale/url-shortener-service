ba9e6aa7d9f60e7950116af962d199865abc8c5f
JesusVale <108861698+JesusVale@users.noreply.github.com>
2025-09-27 13:18:06 -0700

    docker: update docker-compose.yml configuration

--- src/main/java/com/jesga/url_shortener_service/config/RedisConfig.java
+++ src/main/java/com/jesga/url_shortener_service/config/RedisConfig.java
@@ -8,9 +8,12 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.jesga.url_shortener_service.entities.ShortUrl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
@@ -39,6 +42,14 @@ public class RedisConfig {
         return RedisCacheManager
                 .builder(connectionFactory).cacheDefaults(redisCacheConfiguration).build();
 
+    }
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory(Environment env) {
+        String host = env.getRequiredProperty("spring.redis.host");
+        int port = Integer.parseInt(env.getRequiredProperty("spring.redis.port"));
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host, port);
+        return new LettuceConnectionFactory(config);
     }
 
 }
